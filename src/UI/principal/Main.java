afe0516964a7f8eb62d798239ce0579203471cb1
Tray <tmorac@ucenfotec.ac.cr>
2020-08-02 20:44:43 -0600

    Varias escenas creadas

--- src/UI/principal/Main.java
+++ src/UI/principal/Main.java
@@ -1,4 +1,4 @@
-package UI.sample;
+package UI.principal;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,9 +11,9 @@ public class Main extends Application {
     //prueba de comentario 2
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../sample/sample.fxml"));
+        primaryStage.setTitle("CENFO-TECOS");
+        primaryStage.setScene(new Scene(root, 782, 603));
         primaryStage.show();
     }
 
